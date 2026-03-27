0474221f83dfd6bb6d52fc7944d3604d3df9b786
Victor <victorhvilaronga@gmail.com>
2026-03-27 17:53:15 -0300

    Finalizado jogo da forca

---
+++ exercicios_extras/jogo_da_forca.py
@@ -0,0 +1,114 @@
+def existeArquivo(nomeArquivo):
+    """
+    Verifica que o arquivo já existe no projeto
+
+    Args:
+        nomeArquivo str: Nome do arquivo
+    
+    Returns:
+        A conclusão se existe ou não
+    
+    Raises:
+        Aviso de que o arquivo já existe
+    """
+    try:
+        a = open(nomeArquivo, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+def criarArquivo(nomeArquivo):
+    """
+    Esta função cria um arquivo inexistente
+
+    Args:
+        nomeArquivo str: Recebe o nome do arquivo
+    
+    Returns:
+        Cria o arquivo novo
+
+    Raises:
+        Printa mensagem de erro ao criar arquivo.
+    """
+    try:
+        a = open(nomeArquivo, 'wt+')
+        a.close()
+    except:
+        print('Erro na criacao do arquivo')
+    else:
+        print(f'Arquivo {nomeArquivo} criado com sucesso! \n')
+
+def ler_arquivo(nome_arquivo):
+    palavras = []
+    try:
+        with open(nome_arquivo, 'rt') as f:
+            for linha in f:
+                linha = linha.strip()
+                palavra, dica = linha.split(';')
+                palavras.append((palavra, dica))
+    except:
+        print('Erro ao ler o arquivo')
+    return palavras
+
+def jogar(palavra, dica):
+
+    letras_descobertas = ['_'for _ in palavra]
+    letras_tentadas = []
+
+    while '_' in letras_descobertas:
+        print('\nDica: ', dica)
+        print('Palavra: ', ' '.join(letras_descobertas))
+        print('letras já tentadas:', ', '.join(letras_tentadas))
+
+        letra = input('Digite uma letra: ').lower()
+        
+        if letra in letras_tentadas:
+            print('Você já tentou essa letra!')
+            continue
+
+        letras_tentadas.append(letra)
+
+        if letra in palavra:
+            for i in range(len(palavra)):
+                if palavra[i] == letra:
+                    letras_descobertas[i] = letra
+        else:
+            print('Letra nao encontrada!')
+    print('\n Parabéns! Você acertou: ', palavra)
+
+def iniciar_jogo(lista):
+    palavra, dica = random.choice(lista)
+    jogar(palavra, dica)
+
+# PROGRAMA PRINCIPAL
+#imports
+import random
+
+#verificacao de arquivo
+arquivo = 'palavras.txt'
+if existeArquivo(arquivo):
+    print('Arquivo localizado no computador')
+else:
+    print('Arquivado inexistente')
+    criarArquivo(arquivo)
+
+#declaracao de variaveis globais
+lista = ler_arquivo(arquivo)
+
+#logica principal
+print('Bem vindo ao jogo da forca! \n Abaixo selecione para começar: ')
+while True:
+    print('\n1 - Jogar \n 0 - Encerrar')
+    opcao = int(input('Digite a opção: '))
+    
+    if opcao == 1:
+        iniciar_jogo(lista)
+        continuar = input('Jogar novamente? (s/n): ').lower()
+        if continuar != 's':
+            break
+    elif opcao == 0:
+        break
+    else:
+        print('Opção inválida')
